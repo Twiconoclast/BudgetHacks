60f23fba1e9754295ec1e2cbd595d4d388e854b2
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-04-21 16:43:39 -0700

    Fix merge conflicts

--- routes/api/budget_routes.js
+++ routes/api/budget_routes.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
-const passport = require('passport');
-const User = require('../../models/User');
-const validateBudgetInput = require('../../validation/budget');
-
-
-router.patch('/:id', passport.authenticate('jwt', {session: false}),
-  (req, res) => {
-
-  const { errors, isValid } = validateBudgetInput(req.body);
-
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  User.findByIdAndUpdate({_id: req.params.id}, {budget: req.body}, function(err, result){
-      if(err){
-        res.send(err);
-      }else{
-        res.send(result);
-      }
-    }
-  )
-
-
-})
-
-=======
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -65,5 +34,4 @@ router.patch('/:id', passport.authenticate('jwt', {session: false}),
     res.send(user.budget)
   ))})
 
->>>>>>> 290fdca22d247693e9c4dae9a87ff017d14adb1b
   module.exports = router;

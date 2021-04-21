2b62b890a66600bb8cef962e2e83989c7c7f2599
Carol Bares <caroltbares@gmail.com>
2021-04-21 00:07:22 -0700

    prepare for heroku

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.use(express.json());
 app.use("/api/users", users);
 app.use("/api/budget", budget);
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('frontend/build'));
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//   })
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.get("/", (req, res) => res.send("Hello World"));
 app.use(passport.initialize());

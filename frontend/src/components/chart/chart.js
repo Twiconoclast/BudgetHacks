a35e7a0c007b09ceba207fe66597729552ab9060
Raymond Chen <raymondwchen2@gmail.com>
2021-04-22 18:40:00 -0700

    Render data on chart

--- frontend/src/components/chart/chart.js
+++ frontend/src/components/chart/chart.js
@@ -2,13 +2,53 @@ import React from 'react'
 import { Bar } from 'react-chartjs-2';
 
 class SpendingChart extends React.Component{
+
+  total(month){
+    let total = 0;
+    this.props.transactions.forEach((trans)=>{
+      if (trans.category !== 'savings' && trans.category !== 'income'){
+        let monthString = trans.date.slice(5,7);
+  
+        if(monthString === month){
+          total += Number(trans.amount)
+        }
+      }
+    })
+    return -total;
+  }
+
+  getMonth(month){
+    if (month < 10){
+      console.log('in this if statement')
+      month = '0' + month;
+    } else {
+      month = month.toString();
+    }
+    return month;
+  }
+
+  label(month){
+    let monthArr = ['January', 'February', 'March', 
+    'April', 'May', 'June', 'July', 
+    'August', 'September', 'October', 'November', 'December'];
+
+    return monthArr[month]
+  }
+
   render(){
+    let currDate = new Date().getMonth()
+    let Month3Label = this.label(currDate)
+    let Month3Spending = this.total(this.getMonth(currDate + 1))
+    let Month2Label = this.label(currDate-1)
+    let Month2Spending = this.total(this.getMonth(currDate))
+    let Month1Label = this.label(currDate-2)
+    let Month1Spending = this.total(this.getMonth(currDate - 1))
     const data = {
-      labels: ['February', 'March', 'April'],
+      labels: [Month1Label, Month2Label, Month3Label],
       datasets: [
         {
           label: 'Spending',
-          data: [12, 19, 3, 5, 2, 3],
+          data: [Month1Spending, Month2Spending, Month3Spending],
           backgroundColor: [
             'rgba(54, 162, 235, 0.2)',
             'rgba(75, 192, 192, 0.2)',

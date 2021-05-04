b64b6e60f926e84465342e7e33bcfb0143857860
Raymond Chen <raymondwchen2@gmail.com>
2021-05-04 09:34:51 -0700

    fix transaction

--- frontend/src/components/transaction/transaction_index_container.js
+++ frontend/src/components/transaction/transaction_index_container.js
@@ -4,9 +4,12 @@ import {withRouter} from 'react-router-dom'
 import {fetchTransactions, deleteTransaction} from '../../actions/transaction_actions'
 import {fetchUser} from '../../actions/user_actions'
 
+const starter = (a,b) => {
+    return a.date > b.date ? -1 : a.date < b.date ? 1 : 0 }
+
 const mSTP = state => ({
     user: state.session.user,
-    transactions: Object.values(state.transactions),
+    transactions: Object.values(state.transactions).sort(starter),
     balance: state.user.balance
 })
 
